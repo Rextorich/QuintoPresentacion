41607a40383761a4393728e92376976bb0951f2c
Estudiante <Estudiante@DESKTOP-M3LUQ5Q>
2018-10-12 11:25:33 -0500

    Hola

---
+++ src/Suma.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Suma {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println("c");
+    }
+   
+}
